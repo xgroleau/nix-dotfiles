40d6ff72cdfdbce53b173198387519b88fed9264
xgroleau <xavgroleau@gmail.com>
2022-05-13 13:16:27 -0400

    Update to match new flake app format

--- flake.nix
+++ flake.nix
@@ -43,22 +43,29 @@
       let pkgs = import nixpkgs { inherit system; };
       in {
         apps = {
-          fmt = pkgs.writeShellApplication {
-            name = "fmt";
-            runtimeInputs = with pkgs; [ nixfmt statix ];
-            text = ''
-              nixfmt ./**/*.nix
-              statix fix ./.
-            '';
+          fmt = let
+            app = pkgs.writeShellApplication {
+              name = "fmt";
+              runtimeInputs = with pkgs; [ nixfmt statix ];
+              text = ''
+                nixfmt ./**/*.nix
+                statix fix ./.
+              '';
+            };
+          in {
+            type = "app";
+            program = "${app}/bin/${app.name}";
           };
         };
 
         checks = {
+          # TODO: Fix check
           fmt = pkgs.runCommand "fmt" {
             buildInputs = with pkgs; [ nixfmt statix ];
           } ''
             ${pkgs.nixfmt}/bin/nixfmt --check ${./.}/**/*.nix && \
-            ${pkgs.statix}/bin/statix check ${./.}
+            ${pkgs.statix}/bin/statix check ${./.} && \
+            touch $out
           '';
 
         };
