7ca222d0cd8921273d8039165fd18998debc8ef6
xgroleau🐢 <xavgroleau@gmail.com>
2022-11-30 10:11:42 -0500

    Added missing dev deps to vscode for slintlsp

--- home/modules/editors/vscode/default.nix
+++ home/modules/editors/vscode/default.nix
@@ -10,10 +10,27 @@ in {
   config = mkIf cfg.enable {
     programs.vscode = {
       enable = true;
-      package = pkgs.vscode.fhs;
+      # Need some packages for web assembly and preview in IDE
+      package = pkgs.vscode.fhsWithPackages (ps:
+        with ps; [
+          cmake
+          fontconfig
+          libGL
+          openssl.dev
+          pkg-config
+          xorg.libxcb
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXrandr
+          xorg.libXi
+          zlib
+        ]);
+
       extensions = with pkgs.vscode-extensions; [
+        arrterian.nix-env-selector
         bbenoist.nix
         brettm12345.nixfmt-vscode
+        mkhl.direnv
         ms-vscode.cpptools
         tamasfe.even-better-toml
         rust-lang.rust-analyzer
