77385fc021b7a818da858a2d13907337ad92cfc1
xgroleau🐢 <xavgroleau@gmail.com>
2022-12-06 17:57:43 -0500

    bump(flake) bump flake to nixos 22.11 release

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "My user configuration using home-manager";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
 
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/release-22.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
