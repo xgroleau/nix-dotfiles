6ffb3d5cbb9c9a0d1d98197e2c27530741cd03c8
xgroleau🐢 <xavgroleau@gmail.com>
2023-03-13 13:25:34 -0400

    fix(emacs): Removed copilot from emacs.

--- home/modules/editors/emacs/config/config.el
+++ home/modules/editors/emacs/config/config.el
@@ -105,13 +105,3 @@
 ;; Modes settings
 (add-to-list 'auto-mode-alist '("\\.tpp\\'" . c++-mode))
 (add-to-list 'auto-mode-alist '("\\.bzz\\'" . buzz-mode))
-
-
-;; accept completion from copilot and fallback to company
-(use-package! copilot
-  :hook (prog-mode . copilot-mode)
-  :bind (("C-TAB" . 'copilot-accept-completion-by-word)
-         ("C-<tab>" . 'copilot-accept-completion-by-word)
-         :map copilot-completion-map
-         ("<tab>" . 'copilot-accept-completion)
-         ("TAB" . 'copilot-accept-completion)))
