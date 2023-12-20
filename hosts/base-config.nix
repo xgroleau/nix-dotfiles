765a902096b926aa7b1667d45044e8a654ddfb49
xgroleau🐢 <xavgroleau@gmail.com>
2023-12-20 11:19:30 -0500

    feat: moved udev to desktop

--- hosts/base-config.nix
+++ hosts/base-config.nix
@@ -11,30 +11,8 @@
       '';
     };
 
-    services = {
-      udev = {
-        enable = true;
-        packages = with pkgs; [
-          # For embedded
-          stlink
-          qmk-udev-rules
-          openocd
-          libsigrok
-        ];
-        extraRules = ''
-          # Udev rules for nrfconnect
-          # https://github.com/NordicSemiconductor/nrf-udev/blob/dcd4097b4c4c00f1103f94cb8d2faba6437d8101/nrf-udev_1.0.1-all/lib/udev/rules.d/71-nrf.rules
-          ACTION!="add", SUBSYSTEM!="usb_device", GOTO="nrf_rules_end"
-          SUBSYSTEM=="usb", ATTRS{idVendor}=="1915", MODE="0666"
-          KERNEL=="ttyACM[0-9]*", SUBSYSTEM=="tty", SUBSYSTEMS=="usb", ATTRS{idVendor}=="1915", MODE="0666", ENV{NRF_CDC_ACM}="1"
-          LABEL="nrf_rules_end"
-        '';
-      };
-    };
-
     time.timeZone = "America/Toronto";
-
-    environment.systemPackages = with pkgs; [ vim nano curl wget firefox ];
+    environment.systemPackages = with pkgs; [ vim nano curl wget ];
 
     programs.zsh.enable = true;
     i18n.defaultLocale = "en_CA.UTF-8";
--- hosts/desktop.nix
+++ hosts/desktop.nix
@@ -2,6 +2,27 @@
 
 {
   imports = [ ./base-config.nix ];
-  services.udisks2.enable = true;
+
+  services = {
+    udev = {
+      enable = true;
+      packages = with pkgs; [
+        # For embedded
+        stlink
+        qmk-udev-rules
+        openocd
+        libsigrok
+      ];
+      extraRules = ''
+        # Udev rules for nrfconnect
+        # https://github.com/NordicSemiconductor/nrf-udev/blob/dcd4097b4c4c00f1103f94cb8d2faba6437d8101/nrf-udev_1.0.1-all/lib/udev/rules.d/71-nrf.rules
+        ACTION!="add", SUBSYSTEM!="usb_device", GOTO="nrf_rules_end"
+        SUBSYSTEM=="usb", ATTRS{idVendor}=="1915", MODE="0666"
+        KERNEL=="ttyACM[0-9]*", SUBSYSTEM=="tty", SUBSYSTEMS=="usb", ATTRS{idVendor}=="1915", MODE="0666", ENV{NRF_CDC_ACM}="1"
+        LABEL="nrf_rules_end"
+      '';
+    };
+    udisks2.enable = true;
+  };
 
 }
