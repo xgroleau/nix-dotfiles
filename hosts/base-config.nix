65408ab6577713619b6d8630041ff88e3c9b4935
xgroleau🐢 <xavgroleau@gmail.com>
2024-01-11 18:48:34 -0500

    nit: remove zsh for root

--- hosts/base-config.nix
+++ hosts/base-config.nix
@@ -44,7 +44,6 @@
       };
 
       users.root = {
-        shell = pkgs.zsh;
         openssh.authorizedKeys.keys = [
           "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC/ZRV75mh7+1xiTR8+oNDabpUAmUrrEa6drrlhB4H2xqRoaBM5ZGlwuCgB+uTtsdcyM2sf0ZVep9vkjVFDbAAsoSeKM1sIySQXcPjaSFJX51aGUVWorPYfHIVljg6NHFKJtQFow/Kh3lzYs6F7ZbnrSGS25PWiR/ZfJx3RaGpCcyJcDUUjJ0Bt1+ORaayIL429IImEmW0/SqJL3PdzstkS8ukQ8rIki5MTU/Nk7RjbghkmzwONdMbu+8/fego7LbxJYhzdt97lwB0g0k5Z/cSE5Dic3pa2oLRinVyPjfgGyxZ8lugaTjmGB9HroqVfg/C+QWAxUwouX0SWHnCYhXvF xavgroleau@gmail.com"
         ];
