c9474f2abd336ce0ff263a54cdc2e552a98d5e04
Xavier Groleau <xavgroleau@gmail.com>
2024-01-22 11:25:47 -0500

    wip palwolrd

--- modules/palworld-2/default.nix
+++ modules/palworld-2/default.nix
@@ -1,74 +1,81 @@
-# Huge thanks to @Zumorica for creating the initial module:
-# https://github.com/Zumorica/GradientOS/blob/main/hosts/asiyah/palworld-server.nix
 { config, pkgs, lib, ... }:
-
-with lib;
-with lib.my.option;
 let
-  cfg = config.modules.palworld2;
-  steam-id = "2394010";
+  inherit (lib) mdDoc mkIf mkEnableOption mkOption;
+  inherit (lib.types) path port str number;
+  cfg = config.services.palworld;
+  join = builtins.concatStringsSep " ";
 in {
-  options.modules.palworld2 = {
+  imports = [ ];
+
+  options.services.palworld = {
     enable = mkEnableOption "palworld";
-    dataDir = mkReq types.str "Path where the data will be stored";
+    user = mkOption {
+      type = str;
+      default = "palworld";
+      description = mdDoc "User account under which palworld runs";
+    };
+    group = mkOption {
+      type = str;
+      default = "palworld";
+      description = mdDoc "Group under which palworld runs";
+    };
+    dataDir = mkOption {
+      type = path;
+      default = "/var/lib/palworld";
+      description = "where on disk to store your palworld directory";
+    };
     port = mkOption {
-      type = types.port;
+      type = port;
       default = 8211;
       description = "the port to use";
     };
+    maxPlayers = mkOption {
+      type = number;
+      default = 32;
+      description = "the amount of players to support";
+    };
   };
 
-  config = lib.mkIf cfg.enable {
-    systemd.services.palworld = {
-      description = "Palworld Server";
-      wantedBy = [ "multi-user.target" ];
-      after = [ "network.target" ];
-      requires = [ "network.target" ];
-      environment.HOME = cfg.dataDir;
-
-      path = with pkgs; [ steamcmd steam-run ];
-
-      preStart = ''
-        steamcmd \
-          +force_install_dir ${cfg.dataDir} \
-          +login anonymous \
-          +app_update ${steam-id} validate \
-          +quit
-      '';
-
-      script = ''
-        mkdir -p .steam/sdk64/
-        cp linux64/steamclient.so .steam/sdk64/steamclient.so
-        steam-run \
-          ${cfg.dataDir}/PalServer.sh \
-            -useperfthreads \
-            -NoAsyncLoadingThread \
-            -UseMultithreadForDS
-      '';
-
-      serviceConfig = {
-        User = "palworld";
-        Group = "palworld";
-        Restart = "on-failure";
-
-        StateDirectory = "palworld";
-        WorkingDirectory = cfg.dataDir;
-
-        TimeoutStartSec = "1h";
-
-        ProcSubset = "all";
-        RestrictNamespaces = false;
-        SystemCallFilter = [ ];
-      };
-    };
-
-    users.users.palworld = {
-      group = "palworld";
+  config = mkIf cfg.enable {
+    users.users.${cfg.user} = {
+      inherit (cfg) group;
+      home = cfg.dataDir;
+      createHome = true;
       isSystemUser = true;
     };
-    users.groups.palworld = { };
+    users.groups.${cfg.group} = { };
 
-    networking.firewall.allowedTCPPorts = [ cfg.port ];
-    networking.firewall.allowedUDPPorts = [ cfg.port ];
+    systemd.services.palworld = let dir = cfg.dataDir;
+    in {
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        ExecStartPre = join [
+          "${pkgs.steamcmd}/bin/steamcmd"
+          "+force_install_dir ${dir}"
+          "+login anonymous"
+          "+app_update 2394010"
+          "+quit"
+          "&& mkdir -p ${dir}/.steam/sdk64"
+          "&& cp ${dir}/linux64/steamclient.so ${dir}/.steam/sdk64/."
+        ];
+        ExecStart = join [
+          "${pkgs.steam-run}/bin/steam-run ${dir}/Pal/Binaries/Linux/PalServer-Linux-Test Pal"
+          "--port ${toString cfg.port}"
+          "--players ${toString cfg.maxPlayers}"
+          "--useperfthreads"
+          "-NoAsyncLoadingThread"
+          "-UseMultithreadForDS"
+        ];
+        Nice = "-5";
+        Restart = "always";
+        StateDirectory = "palworld";
+        User = cfg.user;
+        WorkingDirectory = cfg.dataDir;
+      };
+      environment = {
+        # linux64 directory is required by palworld.
+        LD_LIBRARY_PATH = "linux64:${pkgs.glibc}/lib";
+      };
+    };
   };
 }
