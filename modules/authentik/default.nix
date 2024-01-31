f247e1c73ba0a66f37610917c8dfb159043ffa51
xgroleau🐢 <xavgroleau@gmail.com>
2024-01-31 09:06:38 -0500

    fix(authentik): var typo

--- modules/authentik/default.nix
+++ modules/authentik/default.nix
@@ -72,7 +72,7 @@ in {
           postgresqlBackup = {
             enable = true;
             backupAll = true;
-            localtion = cfg.backupDir;
+            location = cfg.backupDir;
           };
 
         };
