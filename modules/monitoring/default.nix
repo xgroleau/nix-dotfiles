35589efd5f8f9f1753b0858616b20b9c267eaf16
Xavier Groleau <xavgroleau@gmail.com>
2024-02-18 12:45:22 -0500

    fix: monitoring module

--- modules/monitoring/default.nix
+++ modules/monitoring/default.nix
@@ -66,105 +66,106 @@ in {
       }];
     };
 
-  };
-
-  services.loki = {
-    enable = true;
-    config = {
-      auth_enabled = false;
-      server = {
-        http_listen_port = 3100;
-        grpc_listen_port = 9096;
-      };
-
-      common = {
-        instance_addr = "127.0.0.1";
-        path_prefix = "/tmp/loki";
-        storage = {
-          filesystem = {
-            chunks_directory = "/tmp/loki/chunks";
-            rules_directory = "/tmp/loki/rules";
-          };
+    services.loki = {
+      enable = true;
+      config = {
+        auth_enabled = false;
+        server = {
+          http_listen_port = 3100;
+          grpc_listen_port = 9096;
         };
-        replication_factor = 1;
-        ring = { kvstore.store = "inmemory"; };
-      };
 
-      query_range = {
-        results_cache = {
-          cache = {
-            embedded_cache = {
-              enabled = true;
-              max_size_mb = 100;
+        common = {
+          instance_addr = "127.0.0.1";
+          path_prefix = "/tmp/loki";
+          storage = {
+            filesystem = {
+              chunks_directory = "/tmp/loki/chunks";
+              rules_directory = "/tmp/loki/rules";
+            };
+          };
+          replication_factor = 1;
+          ring = { kvstore.store = "inmemory"; };
+        };
+
+        query_range = {
+          results_cache = {
+            cache = {
+              embedded_cache = {
+                enabled = true;
+                max_size_mb = 100;
+              };
             };
           };
         };
-      };
 
-      schema_config = {
-        config = [{
-          from = "2020-10-24";
-          store = "tsdb";
-          object_store = "filesystem";
-          schema = "v12";
-          index = {
-            prefix = "index_";
-            period = "24h";
-          };
-        }];
-      };
-
-      limits_config = {
-        reject_old_samples = true;
-        reject_old_samples_max_age = "168h";
-      };
-
-      chunk_store_config = { max_look_back_period = "0s"; };
-
-      table_manager = {
-        retention_deletes_enabled = false;
-        retention_period = "0s";
-      };
-
-      compactor = {
-        working_directory = "/var/lib/loki";
-        shared_store = "filesystem";
-        compactor_ring = { kvstore = { store = "inmemory"; }; };
-      };
-
-      reporting_enabled = false;
-    };
-  };
-
-  services.promtail = {
-    enable = true;
-    config = {
-      server = {
-        http_listen_port = 28183;
-        grpc_listen_port = 0;
-
-      };
-      positions = { filename = /tmp/positions.yaml; };
-      clients = [{
-        url = "http://127.0.0.1:${
-            toString config.services.loki.configuration.server.http_listen_port
-          }/loki/api/v1/push";
-      }];
-      scrape_configs = [{
-        job_name = "journal";
-        journal = {
-          max_age = "12h";
-          labels = {
-            job = "systemd-journal";
-            host = hostname;
-          };
+        schema_config = {
+          config = [{
+            from = "2020-10-24";
+            store = "tsdb";
+            object_store = "filesystem";
+            schema = "v12";
+            index = {
+              prefix = "index_";
+              period = "24h";
+            };
+          }];
         };
-        relabel_configs = [{
-          source_labels = [ "__journal__systemd_unit" ];
-          target_label = "unit";
-        }];
 
-      }];
+        limits_config = {
+          reject_old_samples = true;
+          reject_old_samples_max_age = "168h";
+        };
+
+        chunk_store_config = { max_look_back_period = "0s"; };
+
+        table_manager = {
+          retention_deletes_enabled = false;
+          retention_period = "0s";
+        };
+
+        compactor = {
+          working_directory = "/var/lib/loki";
+          shared_store = "filesystem";
+          compactor_ring = { kvstore = { store = "inmemory"; }; };
+        };
+
+        reporting_enabled = false;
+      };
+    };
+
+    services.promtail = {
+      enable = true;
+      config = {
+        server = {
+          http_listen_port = 28183;
+          grpc_listen_port = 0;
+
+        };
+        positions = { filename = /tmp/positions.yaml; };
+        clients = [{
+          url = "http://127.0.0.1:${
+              toString
+              config.services.loki.configuration.server.http_listen_port
+            }/loki/api/v1/push";
+        }];
+        scrape_configs = [{
+          job_name = "journal";
+          journal = {
+            max_age = "12h";
+            labels = {
+              job = "systemd-journal";
+              host = hostname;
+            };
+          };
+          relabel_configs = [{
+            source_labels = [ "__journal__systemd_unit" ];
+            target_label = "unit";
+          }];
+
+        }];
+      };
+
     };
 
   };
