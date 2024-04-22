047927b401de34f3535863e045ceecfe45c3e5b3
Xavier Groleau <xavgroleau@gmail.com>
2024-04-22 18:59:59 -0400

    fix: valheim

--- modules/valheim/default.nix
+++ modules/valheim/default.nix
@@ -102,7 +102,7 @@ in {
           };
 
           environment = {
-            LD_LIBRARY_PATH = with pkgs; "/var/lib/steam-app-${steam-app}/linux64:${glibc}/lib";
+			      LD_LIBRARY_PATH = "linux64:${pkgs.glibc}/lib";
 			      SteamAppId = "892970";
           };
         };
