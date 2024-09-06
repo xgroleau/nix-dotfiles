7a2665cf84f1ff6cd108020d3f52287c7d4f0a87
xgroleau🐢 <xavgroleau@gmail.com>
2024-09-06 10:06:20 -0400

    feat: added attic cache

--- home/modules/nix/default.nix
+++ home/modules/nix/default.nix
@@ -12,7 +12,6 @@ in
 
   options.modules.nix = {
     caches = lib.mkEnableOption "Enable common caches";
-    attic = lib.mkEnableOption "Enable selfhosted attic cache";
     builders = lib.mkEnableOption "Enable remote builders";
   };
 
@@ -21,26 +20,23 @@ in
       (lib.mkIf cfg.caches {
         substituters = [
           "https://cache.nixos.org/?priority=40"
-          "https://nix-community.cachix.org?priority=41" # this is a useful public cache!
-          "https://numtide.cachix.org?priority=42" # this is also a useful public cache!
+          "https://nix-community.cachix.org?priority=41"
+          "https://numtide.cachix.org?priority=42"
+          "http://sheogorath:15000/xgroleau?priority=43"
         ];
         trusted-public-keys = [
           "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+          "xgroleau:+u4lhV57NLS42SO5JWIURSLg2TaIbWUPe5dYXJojfRA="
         ];
+        netrc-file = "${config.xdg.configHome}/nix/netrc";
       })
 
       (lib.mkIf cfg.builders {
         builders = ''ssh-ng://builder@sheogorath x86_64-linux - 1 2 nixos-test,benchmark,big-parallel,kvm - -; ssh-ng://builder@jyggalag aarch64-linux - 1 2 - - -'';
       })
 
-      (lib.mkIf cfg.attic {
-        # TODO:
-        # substituters = [ "http://localhost:8080/xgroleau" ];
-        # trusted-public-keys = [ "xgroleau:TFq4PFa5qGGFCPHmvwz94TP++W8hZAmoxopPWV6k7Ok=" ];
-        netrc-file = "${config.xdg.configHome}/nix/netrc";
-      })
     ];
   };
 }
