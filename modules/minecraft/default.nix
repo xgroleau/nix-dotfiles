49800b4fbbfa99a5a6f456fbe487c113503560e6
xgroleau <xavgroleau@gmail.com>
2024-09-20 18:03:25 -0400

    wip

--- modules/minecraft/default.nix
+++ modules/minecraft/default.nix
@@ -36,88 +36,80 @@ in
 
   };
 
-  config = lib.mkIf cfg.enable {
-    services.minecraft-servers = {
-      enable = true;
-      dataDir = "${cfg.dataDir}/data";
-      runDir = "${cfg.dataDir}";
+  config =
+    lib.mkIf cfg.enable
 
-      eula = true;
+      {
+        services.minecraft-servers = {
+          enable = true;
+          dataDir = "${cfg.dataDir}/data";
+          runDir = "${cfg.dataDir}";
 
-      servers."${cfg.name}" = {
-        enable = true;
-        autoStart = true;
-        restart = "always";
-        openFirewall = cfg.openFirewall;
-        package = pkgs.fabricServers.fabric-1_21_1.override { loaderVersion = "0.15.11"; };
+          eula = true;
 
-        serverProperties = {
-          enable-command-block = true;
-          enforce-whitelist = true;
-          gamemode = "survival";
-          generate-structures = "true";
-          hide-online-players = false;
-          initial-enabled-packs = "vanilla";
-          level-name = cfg.name;
-          motd = "${cfg.name} server :)";
-          online-mode = false;
-          pvp = true;
-          require-resource-pack = false;
-          # resource-pack=
-          # resource-pack-id=
-          # resource-pack-prompt=
-          # resource-pack-sha1=
-          server-port = cfg.port;
-          white-list = false;
+          servers."${cfg.name}" =
+            let
+              modpack = pkgs.fetchPackwizModpack {
+                url = "https://gitlab.com/xavgroleau/yofo-mc-mods/-/blob/v1.0.0/pack.toml?ref_type=tags";
+                packHash = "sha256-L5RiSktqtSQBDecVfGj1iDaXV+E90zrNEcf4jtsg+wk=";
+              };
+              mcVersion = modpack.manifest.versions.minecraft;
+              fabricVersion = modpack.manifest.versions.fabric;
+              serverVersion = lib.replaceStrings [ "." ] [ "_" ] "fabric-${mcVersion}";
+            in
+            {
+
+              enable = true;
+              autoStart = true;
+              restart = "always";
+              openFirewall = cfg.openFirewall;
+
+              package = pkgs.fabricServers.${serverVersion}.override { loaderVersion = fabricVersion; };
+              symlinks = {
+                "mods" = "${modpack}/mods";
+              };
+
+              serverProperties = {
+                enable-command-block = true;
+                enforce-whitelist = true;
+                gamemode = "survival";
+                generate-structures = "true";
+                hide-online-players = false;
+                initial-enabled-packs = "vanilla";
+                level-name = cfg.name;
+                motd = "${cfg.name} server :)";
+                online-mode = false;
+                pvp = true;
+                require-resource-pack = false;
+                # resource-pack=
+                # resource-pack-id=
+                # resource-pack-prompt=
+                # resource-pack-sha1=
+                server-port = cfg.port;
+                white-list = false;
+              };
+            };
+
         };
-        symlinks = {
-          mods = pkgs.linkFarmFromDrvs "mods" (
-            builtins.attrValues {
-              frabricAPI = pkgs.fetchurl {
-                url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/qKPgBeHl/fabric-api-0.104.0%2B1.21.1.jar";
-                sha512 = "0773f45d364b506b4e5b024aa8f1d498900fcf0a020d2025154e163e50a0eeee1b8296bf29c21df5ced42126ed46635e5ed094df25796ec552eb76399438e7e7";
+        # Create the folder if it doesn't exist
+        systemd.tmpfiles.settings."mineraft-${cfg.name}" = {
+          "${cfg.dataDir}" = {
+            d = {
+              user = "minecraft";
+              group = "minecraft";
+              mode = "755";
+            };
+          };
 
-              };
-
-              # VPC dep
-              geckoLib = pkgs.fetchurl {
-                url = "https://cdn.modrinth.com/data/8BmcQJ2H/versions/5jcq814u/geckolib-fabric-1.21.1-4.6.5.jar";
-                sha512 = "f8275fc0fe9cdd9e598bf3b0ad94313f89092abb95c5a03ae3f37ac1a24a01d6630d1af962079b264ec2f976ef581f134ca0dd0c7b95ebf6242efb2f9472d28f";
-              };
-              vicPointBlank = pkgs.fetchurl {
-                url = "https://cdn.modrinth.com/data/og4KPYmA/versions/HiwllvyQ/pointblank-fabric-1.21-1.6.7.jar";
-                sha512 = "d02b4e037c2a5863978f2a7535c920b3fe39fcb6b7603c1c23e0597daad41ab038c014aa02d0414aa4c2b1a67d237c575458d70adee89cc0e2b8f8967e3d8efd";
-              };
-
-              appleSkin = pkgs.fetchurl {
-                url = "https://cdn.modrinth.com/data/EsAfCjCV/versions/fG1xJao0/appleskin-fabric-mc1.21-3.0.5.jar";
-                sha512 = "32176384779f6e223ce6c68bfa3c505222be0d5a21606a195562b471d29b2f7af253bb42756dd66373475bb75582fc11c72d8bd2f311bd0f0ad06816e4f61a29";
-              };
-            }
-          );
+          "${cfg.dataDir}/data" = {
+            d = {
+              user = "minecraft";
+              group = "minecraft";
+              mode = "755";
+            };
+          };
         };
+
       };
-
-    };
-    # Create the folder if it doesn't exist
-    systemd.tmpfiles.settings."mineraft-${cfg.name}" = {
-      "${cfg.dataDir}" = {
-        d = {
-          user = "minecraft";
-          group = "minecraft";
-          mode = "755";
-        };
-      };
-
-      "${cfg.dataDir}/data" = {
-        d = {
-          user = "minecraft";
-          group = "minecraft";
-          mode = "755";
-        };
-      };
-    };
-
-  };
 
 }
