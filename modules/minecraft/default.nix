ba939261257a4e030d50e31933ee4e098e1f1e4c
xgroleau <xavgroleau@gmail.com>
2024-09-20 07:48:12 -0500

    wip

--- modules/minecraft/default.nix
+++ modules/minecraft/default.nix
@@ -73,10 +73,21 @@ in
         symlinks = {
           mods = pkgs.linkFarmFromDrvs "mods" (
             builtins.attrValues {
-              # vicPointBlank = pkgs.fetchurl {
-              #   url = "https://cdn.modrinth.com/data/og4KPYmA/versions/HiwllvyQ/pointblank-fabric-1.21-1.6.7.jar";
-              #   sha512 = "d02b4e037c2a5863978f2a7535c920b3fe39fcb6b7603c1c23e0597daad41ab038c014aa02d0414aa4c2b1a67d237c575458d70adee89cc0e2b8f8967e3d8efd";
-              # };
+              frabricAPI = pkgs.fetchurl {
+                url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/qKPgBeHl/fabric-api-0.104.0%2B1.21.1.jar";
+                sha512 = "0773f45d364b506b4e5b024aa8f1d498900fcf0a020d2025154e163e50a0eeee1b8296bf29c21df5ced42126ed46635e5ed094df25796ec552eb76399438e7e7";
+              };
+
+              vicPointBlank = pkgs.fetchurl {
+                url = "https://cdn.modrinth.com/data/og4KPYmA/versions/HiwllvyQ/pointblank-fabric-1.21-1.6.7.jar";
+                sha512 = "d02b4e037c2a5863978f2a7535c920b3fe39fcb6b7603c1c23e0597daad41ab038c014aa02d0414aa4c2b1a67d237c575458d70adee89cc0e2b8f8967e3d8efd";
+              };
+              # VicPointBlank dep
+              geckoLib = pkgs.fetchurl {
+                url = "https://cdn.modrinth.com/data/8BmcQJ2H/versions/DCJ4wE6H/geckolib-forge-1.21.1-4.6.5.jar";
+                sha512 = "f74bbf02828aae3bc1198f2cc972c913d0a896744af9290b4419193f29c3e7a2852ffc11991245c1238baee102c4f2476d37d87434334dc551b5e8cb27ac0eca";
+              };
+
               appleSkin = pkgs.fetchurl {
                 url = "https://cdn.modrinth.com/data/EsAfCjCV/versions/fG1xJao0/appleskin-fabric-mc1.21-3.0.5.jar";
                 sha512 = "32176384779f6e223ce6c68bfa3c505222be0d5a21606a195562b471d29b2f7af253bb42756dd66373475bb75582fc11c72d8bd2f311bd0f0ad06816e4f61a29";
