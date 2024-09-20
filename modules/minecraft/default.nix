f2f642c703b3baaec4843d18ce0b975a751a28ec
xgroleau <xavgroleau@gmail.com>
2024-09-19 21:26:03 -0500

    wip

--- modules/minecraft/default.nix
+++ modules/minecraft/default.nix
@@ -47,10 +47,9 @@ in
       servers."${cfg.name}" = {
         enable = true;
         autoStart = true;
-        # # enableReload = true;
         restart = "always";
         openFirewall = cfg.openFirewall;
-        package = pkgs.vanillaServers.vanilla-1_21;
+        package = pkgs.fabric-1_18_2.override { loaderVersion = "0.15.11"; };
 
         serverProperties = {
           enable-command-block = true;
@@ -71,16 +70,16 @@ in
           server-port = cfg.port;
           white-list = false;
         };
-        # symlinks = {
-        #   mods = pkgs.linkFarmFromDrvs "mods" (
-        #     builtins.attrValues {
-        #       vicPointBlank = pkgs.fetchurl {
-        #         url = "https://cdn.modrinth.com/data/og4KPYmA/versions/HiwllvyQ/pointblank-fabric-1.21-1.6.7.jar";
-        #         sha512 = "d02b4e037c2a5863978f2a7535c920b3fe39fcb6b7603c1c23e0597daad41ab038c014aa02d0414aa4c2b1a67d237c575458d70adee89cc0e2b8f8967e3d8efd";
-        #       };
-        #     }
-        #   );
-        # };
+        symlinks = {
+          mods = pkgs.linkFarmFromDrvs "mods" (
+            builtins.attrValues {
+              vicPointBlank = pkgs.fetchurl {
+                url = "https://cdn.modrinth.com/data/og4KPYmA/versions/HiwllvyQ/pointblank-fabric-1.21-1.6.7.jar";
+                sha512 = "d02b4e037c2a5863978f2a7535c920b3fe39fcb6b7603c1c23e0597daad41ab038c014aa02d0414aa4c2b1a67d237c575458d70adee89cc0e2b8f8967e3d8efd";
+              };
+            }
+          );
+        };
       };
 
     };
