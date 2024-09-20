811d6fa52a1360eef793026cacfc0f2b4fe19c9c
xgroleau <xavgroleau@gmail.com>
2024-09-20 07:33:12 -0500

    wip

--- modules/minecraft/default.nix
+++ modules/minecraft/default.nix
@@ -70,16 +70,20 @@ in
           server-port = cfg.port;
           white-list = false;
         };
-        # symlinks = {
-        #   mods = pkgs.linkFarmFromDrvs "mods" (
-        #     builtins.attrValues {
-        #       vicPointBlank = pkgs.fetchurl {
-        #         url = "https://cdn.modrinth.com/data/og4KPYmA/versions/HiwllvyQ/pointblank-fabric-1.21-1.6.7.jar";
-        #         sha512 = "d02b4e037c2a5863978f2a7535c920b3fe39fcb6b7603c1c23e0597daad41ab038c014aa02d0414aa4c2b1a67d237c575458d70adee89cc0e2b8f8967e3d8efd";
-        #       };
-        #     }
-        #   );
-        # };
+        symlinks = {
+          mods = pkgs.linkFarmFromDrvs "mods" (
+            builtins.attrValues {
+              # vicPointBlank = pkgs.fetchurl {
+              #   url = "https://cdn.modrinth.com/data/og4KPYmA/versions/HiwllvyQ/pointblank-fabric-1.21-1.6.7.jar";
+              #   sha512 = "d02b4e037c2a5863978f2a7535c920b3fe39fcb6b7603c1c23e0597daad41ab038c014aa02d0414aa4c2b1a67d237c575458d70adee89cc0e2b8f8967e3d8efd";
+              # };
+              appleSkin = pkgs.fetchurl {
+                url = "https://cdn.modrinth.com/data/EsAfCjCV/versions/fG1xJao0/appleskin-fabric-mc1.21-3.0.5.jar";
+                sha512 = "32176384779f6e223ce6c68bfa3c505222be0d5a21606a195562b471d29b2f7af253bb42756dd66373475bb75582fc11c72d8bd2f311bd0f0ad06816e4f61a29";
+              };
+            }
+          );
+        };
       };
 
     };
