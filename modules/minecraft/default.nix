961271511b10f649b89c5c8c200452ab7e1c30e3
xgroleau <xavgroleau@gmail.com>
2024-09-19 21:30:35 -0500

    wip

--- modules/minecraft/default.nix
+++ modules/minecraft/default.nix
@@ -39,47 +39,47 @@ in
   config = lib.mkIf cfg.enable {
     services.minecraft-servers = {
       enable = true;
-      dataDir = "${cfg.dataDir}/data";
-      runDir = "${cfg.dataDir}";
+      # dataDir = "${cfg.dataDir}/data";
+      # runDir = "${cfg.dataDir}";
 
       eula = true;
 
       servers."${cfg.name}" = {
         enable = true;
-        autoStart = true;
-        restart = "always";
-        openFirewall = cfg.openFirewall;
-        package = pkgs.fabricServers.fabric-1_21.override { loaderVersion = "0.15.11"; };
+        # autoStart = true;
+        # restart = "always";
+        # openFirewall = cfg.openFirewall;
+        # package = pkgs.fabricServers.fabric-1_21.override { loaderVersion = "0.15.11"; };
 
-        serverProperties = {
-          enable-command-block = true;
-          enforce-whitelist = true;
-          gamemode = "survival";
-          generate-structures = "true";
-          hide-online-players = false;
-          initial-enabled-packs = "vanilla";
-          level-name = cfg.name;
-          motd = "${cfg.name} server :)";
-          online-mode = false;
-          pvp = true;
-          require-resource-pack = false;
-          # resource-pack=
-          # resource-pack-id=
-          # resource-pack-prompt=
-          # resource-pack-sha1=
-          server-port = cfg.port;
-          white-list = false;
-        };
-        symlinks = {
-          mods = pkgs.linkFarmFromDrvs "mods" (
-            builtins.attrValues {
-              vicPointBlank = pkgs.fetchurl {
-                url = "https://cdn.modrinth.com/data/og4KPYmA/versions/HiwllvyQ/pointblank-fabric-1.21-1.6.7.jar";
-                sha512 = "d02b4e037c2a5863978f2a7535c920b3fe39fcb6b7603c1c23e0597daad41ab038c014aa02d0414aa4c2b1a67d237c575458d70adee89cc0e2b8f8967e3d8efd";
-              };
-            }
-          );
-        };
+        # serverProperties = {
+        #   enable-command-block = true;
+        #   enforce-whitelist = true;
+        #   gamemode = "survival";
+        #   generate-structures = "true";
+        #   hide-online-players = false;
+        #   initial-enabled-packs = "vanilla";
+        #   level-name = cfg.name;
+        #   motd = "${cfg.name} server :)";
+        #   online-mode = false;
+        #   pvp = true;
+        #   require-resource-pack = false;
+        #   # resource-pack=
+        #   # resource-pack-id=
+        #   # resource-pack-prompt=
+        #   # resource-pack-sha1=
+        #   server-port = cfg.port;
+        #   white-list = false;
+        # };
+        # symlinks = {
+        #   mods = pkgs.linkFarmFromDrvs "mods" (
+        #     builtins.attrValues {
+        #       vicPointBlank = pkgs.fetchurl {
+        #         url = "https://cdn.modrinth.com/data/og4KPYmA/versions/HiwllvyQ/pointblank-fabric-1.21-1.6.7.jar";
+        #         sha512 = "d02b4e037c2a5863978f2a7535c920b3fe39fcb6b7603c1c23e0597daad41ab038c014aa02d0414aa4c2b1a67d237c575458d70adee89cc0e2b8f8967e3d8efd";
+        #       };
+        #     }
+        #   );
+        # };
       };
 
     };
