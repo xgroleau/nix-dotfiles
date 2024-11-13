9d4b215f4cba81855a19bd7e331fd04d173ed626
xgroleau <xavgroleau@gmail.com>
2024-11-13 11:42:44 -0500

    nit: fix ollama package

--- modules/ollama/default.nix
+++ modules/ollama/default.nix
@@ -29,7 +29,7 @@ in
     lib.mkIf cfg.enable {
       services.ollama = {
         enable = true;
-        pkg = pkgs.unstable.ollama;
+        package = pkgs.unstable.ollama;
         listenAddress = "0.0.0.0:${toString cfg.port}";
         # TODO: loadModels = ["llama3.2"];
       };
