076d6384bd6df38b42d7fb94c2119cda2a5af5d7
xgroleau <xavgroleau@gmail.com>
2024-12-05 12:28:22 -0500

    nit: force mealie

--- modules/mealie/default.nix
+++ modules/mealie/default.nix
@@ -71,7 +71,7 @@ in
     systemd.services.mealie = {
       serviceConfig = {
         ReadWritePaths = [ cfg.dataDir ];
-        StateDirectory = null;
+        StateDirectory = lib.mkForce (null);
       };
 
     };
